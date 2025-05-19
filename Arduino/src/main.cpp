c428267238700872990c15be3d74bf197d0ae1f4
mjorgecruz <manueljorgecruz@hotmail.com>
2025-05-19 13:06:46 +0100

    Fix CAN initialization and adjust interval for pulse counting

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -4,7 +4,7 @@
 #include <Wire.h>
 
 
-MCP_CAN CAN(9);
+MCP_CAN CAN(10);
 SimpleTimer timer;
 byte data[1] = {20};
 byte len = 0;
@@ -13,7 +13,7 @@ unsigned long rxId;
 
 
 const int sensorPin = 3;  
-const long interval = 300;  
+const long interval = 10;  
 
 const float wheelDiameter = 0.067; 
 float circumference = 3.14 * wheelDiameter;
@@ -21,7 +21,7 @@ float alpha = 0.5; // Smoothing factor (0 < alpha <= 1)
 //float smoothedSpeed = 0;
 
 const int pulsesPerRevolution = 10;
-unsigned long pulseCount = 0;
+int pulseCount = 0;
 
 unsigned long previousMillisLeft = 0;
 unsigned long previousMillisRight = 0;
@@ -31,7 +31,7 @@ const int rightBlinkerPin = 6;
 const int BeamPin = 11;
 const int rearLightPin = 7;
 const int rearFogLightPin = 8;
-const int frontFogLightPin = 10;
+const int frontFogLightPin = 4;
 const int parkingLightPin = 15;
 const int buzzer_pin = 12;
 const int pins[8] = {leftBlinkerPin, rightBlinkerPin, rearLightPin, rearFogLightPin, frontFogLightPin, BeamPin, parkingLightPin, buzzer_pin};
@@ -46,19 +46,31 @@ bool rearfogLight = false;
 bool frontfogLight = false;
 bool buzz = false;
 
+// Add these global variables
+unsigned long lastPulseTime = 0;
+unsigned long currentPulseTime = 0;
+volatile unsigned long pulsePeriod = 0;
+volatile boolean newPulseDetected = false;
+float rpm = 0;
+
 void countPulse() {
+    currentPulseTime = micros();
+    pulsePeriod = currentPulseTime - lastPulseTime;
+	rpm = 60000000.0 / (pulsePeriod * pulsesPerRevolution);
+    lastPulseTime = currentPulseTime;
 	pulseCount++;
 }
 
+
 void sendCANMessage() {
-	float rps = (float)pulseCount / pulsesPerRevolution / (interval / 1000.0);
-	//smoothedSpeed = alpha * rps + (1 - alpha) * smoothedSpeed;
-	float rpm = rps * 60; // Convert to RPM
-	pulseCount = 0;
 
-	Serial.print("RPM: ");
-	Serial.println(rpm);
+    Serial.print("RPM: ");
+    Serial.println(rpm);
 
+
+	if (micros() - lastPulseTime > 100000) {
+        rpm = 0;
+    }
 	int speed = (int) rpm;
 
 	byte data[8];
@@ -198,7 +210,7 @@ void setup()
 
 	Serial.println("CAN transmitter test");
 	 
-	if(CAN.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) 
+	if(CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) 
 		Serial.println("MCP2515 Initialized Successfully!");
 	else 
 		Serial.println("Error Initializing MCP2515...");
@@ -224,100 +236,3 @@ void loop()
 	checkLoop();
 
 }
-
-
-
-
-// int reading = 0;
-
-// void setup() {
-//   Wire.begin();   
-//   pinMode(leftBlinkerPin, OUTPUT);
-//   pinMode(rightBlinkerPin, OUTPUT);
-//   pinMode(frontLight1, OUTPUT);
-//   pinMode(frontLight2, OUTPUT);
-//   pinMode(readFogLight, OUTPUT);  
-//   pinMode(buzzer_pin, OUTPUT); 
-//   Serial.begin(9600);
-// }
-
-
-
-
-
-// void controlFrontLights(bool highBeam, bool mediumBeam, bool lowBeam) {
-//   if (highBeam) {
-//     analogWrite(frontLight1, 255);
-//     analogWrite(frontLight2, 255);
-//   } else if (mediumBeam) {
-//     analogWrite(frontLight1, 128);
-//     analogWrite(frontLight2, 128);
-//   } else if (lowBeam) {
-//     analogWrite(frontLight1, 64);
-//     analogWrite(frontLight2, 64);
-//   } else {
-//     analogWrite(frontLight1, 0);
-//     analogWrite(frontLight2, 0);
-//   }
-// }
-
-// void controlRearLights(bool rearLights) {
-//   if (rearLights) {
-//     analogWrite(rearLight1, 255);
-//     analogWrite(rearLight2, 255);
-//   } else {
-//     analogWrite(rearLight1, 0);
-//     analogWrite(rearLight2, 0);
-//   }
-// }
-
-// void checkDistance(bool buzz) {
-//   Wire.beginTransmission(112);
-//   Wire.write(byte(0x00));
-//   Wire.write(byte(0x51)); 
-//   Wire.endTransmission();
-//   delay(70); 
-
-//   Wire.beginTransmission(112);
-//   Wire.write(0x02);
-//   Wire.write(0x24);
-//   Wire.endTransmission();
-	
-//   Wire.beginTransmission(112);
-//   Wire.write(0x02); 
-//   Wire.endTransmission();
-
-//   Wire.requestFrom(112, 2); 
-//   if (2 <= Wire.available()) {
-//     reading = Wire.read();
-//     reading = reading << 8;
-//     reading |= Wire.read();
-//     Serial.println(reading);
-
-//     if (reading < 5 && buzz) {
-//       digitalWrite(buzzer_pin, HIGH);
-//     } else {
-//       digitalWrite(buzzer_pin, LOW);
-//     }
-//   }
-// }
-
-// void loop() {
-//   bool leftBlinker = true;
-//   bool rightBlinker = false;
-//   bool highBeam = true;
-//   bool mediumBeam = false;
-//   bool lowBeam = false;
-//   bool rearLights = true;
-//   bool fogLight = true;
-//   bool buzz = false;
-
-
-	
-//   controlFrontLights(highBeam, mediumBeam, lowBeam);
-//   controlRearLights(rearLights);
-	
-//   if (fogLight) digitalWrite(readFogLight, HIGH);
-
-//   checkDistance(buzz);
-// }
