303cf0336c7069f797e930487951d960051e0afa
Luis Filipe Carvalho <116769182+luis-ffe@users.noreply.github.com>
2025-06-30 14:20:38 +0000

    Updated Car Radio transmissio frequency and stopBits logic - Working Correctly now less interference

--- Kitronik/car/car.py
+++ Kitronik/car/car.py
@@ -1,8 +1,12 @@
 radio.on()
 radio.set_group(7)
 
+
+#play with radio transmission power - strenght rssi increrses more easly detectable maybe too easly 
+# radio.set_transmit_power(7)
+
 def on_forever():
     radio.send_string("on")
-    basic.pause(5)
+    basic.pause(50)
 
 basic.forever(on_forever)
--- Kitronik/stopBit/trafficLightIntersection.py
+++ Kitronik/stopBit/trafficLightIntersection.py
@@ -1,4 +1,3 @@
-
 #################################################################################
 #                                                                               #
 #                      ****    RULES / STANDARDS    ****                        #
@@ -6,7 +5,7 @@
 #################################################################################
 
 # Cluster 1 is a group of Traffic lights that work together and comunicate.
-# It is required for all the Units of this intersection to be on the same 
+# It is required for all the Units of this intersection to be on the same
 #cluster and Radio Group.
 # Each unit should have a unique name / ID.
 # Ex: 3 Traffic lights:  A, B and C   in the cluster 1:
@@ -16,22 +15,25 @@
 
 #################################################################################
 
-ID = "C1A" 
+ID = "C1A"
 NEXT_ID = "C1B"
 CAR_ID = "on"
+
+
 #radio Group = Radio Frequency Channel
 GROUP = 7
 # each listening for activation token turn duration 2s
-TURN_DURATION = 2000
-# time the Green light is kept on 10s
-GREEN_DURATION = 10000
-YELLOW_DURATION = 3000
+TURN_DURATION = 600
+# time the Green light is kept on 8s orange 2s
+GREEN_DURATION = 2000
+YELLOW_DURATION = 1000
+
 # Radio Signal Strength Indicator
-RSSI_THRESHOLD = -50
+RSSI_THRESHOLD = -44
+MANUAL_LIGHT = ""
 
 # if after 15 seconds the turn is stuck this will terminate it
 # security redundancy
-
 FAILSAFE_TIME = 15000
 
 # program variables
@@ -44,30 +46,22 @@ turn_start_time = 0
 auto_mode = True
 #car detected status
 detected = False
-
+elapsed = 0
 
 def on_received_string(token):
-    global my_turn, last_token, turn_start_time
+    global my_turn, last_token, turn_start_time, rssi, last_signal_time, detected
     last_token = token
 
-    if my_turn == False:
-        if token == ID:
-            my_turn = True
-            turn_start_time = input.running_time()
-
-
-# Checking for proximity only if the message is "on" meaning it is the car not any signal
-def check_car_nearby():
-    global rssi, last_signal_time
-    message = radio.receive_string()
+    if my_turn == False and token == ID:
+        my_turn = True
+        turn_start_time = input.running_time()
 
     #only measure RSSI if the detected signal is the car
-    if message == CAR_ID:
+    if my_turn and last_token == CAR_ID:
         rssi = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
         if rssi > RSSI_THRESHOLD:
             last_signal_time = input.running_time()
-            return True
-    return False
+            detected = True
 
 
 #traffic lights setter
@@ -86,76 +80,96 @@ def show_mode():
         basic.show_string("M")
 
 def on_forever():
-    global my_turn, state, turn_start_time, detected
+    global my_turn, state, turn_start_time, detected, elapsed
     now = input.running_time()
 
     if my_turn:
         elapsed = now - turn_start_time
 
-        # Failsafe
-        if elapsed > FAILSAFE_TIME:
-            my_turn = False
-            set_light(True, False, False)
-            state = "red"
-            basic.show_string("F")
-            detected = False
-            return
-
         # check for car during TURN_DURATION
-        if elapsed < TURN_DURATION and detected == False:
-            if check_car_nearby():
-                detected = True
+        if elapsed < TURN_DURATION:
+            if detected == True:
                 set_light(False, False, True)
                 state = "green"
                 basic.pause(GREEN_DURATION)
                 set_light(False, True, False)
                 state = "yellow"
                 basic.pause(YELLOW_DURATION)
-
+                detected = False
+            
+            elapsed = now - turn_start_time
+            
         # After TURN_DURATION or after car detection loop end turn
-        elif elapsed >= TURN_DURATION:
+        if elapsed >= TURN_DURATION:
             set_light(True, False, False)
             state = "red"
-
-            # send token NEXT_ID for x milliseconds (redundancy)
-            send_start = input.running_time()
-            while input.running_time() - send_start < 1000:
-                radio.send_string(NEXT_ID)
-                basic.pause(200)
             my_turn = False
             detected = False
-            return
+                
+            # send token NEXT_ID for x milliseconds (redundancy)
+            send_start = input.running_time()
+            while input.running_time() - send_start <= 150:
+                radio.send_string(NEXT_ID)
+                basic.pause(20)
 
-    basic.pause(50)
+    basic.pause(12)
+    #show_mode()
+
+
+last_switch_time = 0
+
+def cycle_lights_non_blocking():
+    global last_switch_time, state
+
+    now = input.running_time()
+    if now - last_switch_time >= 10000:
+        if state == "red":
+            set_light(False, False, True)
+            state = "green"
+        elif state == "green":
+            set_light(False, True, False)
+            state = "yellow"
+        elif state == "yellow":
+            set_light(True, False, False)
+            state = "red"
+        last_switch_time = now
 
 # button A starts the token loop manually
 def on_button_a_pressed():
     global detected
-    if not auto_mode:
-        radio.send_string(ID)
-        detected = False
-
-# Button B toggles auto/manual mode
-def on_button_b_pressed():
-    global auto_mode
-    auto_mode = not auto_mode
+    detected = False
+    basic.show_number(1)
+    radio.send_string(NEXT_ID)
     show_mode()
 
-# A and B pressed Shows RSSI
+
+# A&B pressed: Shows RSSI, then manually cycles traffic lights (green → yellow → red)
 def on_button_ab_pressed():
-    basic.show_number(rssi)
+    global MANUAL_LIGHT
+
+    if MANUAL_LIGHT == "":
+        basic.show_number(rssi)
+        MANUAL_LIGHT = "rssi"
+        return
+
+    if MANUAL_LIGHT == "rssi":
+        basic.show_string("L")
+        cycle_lights_non_blocking()
+        MANUAL_LIGHT = ""
+        show_mode()
 
 # Main Program:
+
 # Set radio group and turn antena on.
-radio.set_group(GROUP)
 radio.on()
+radio.set_group(GROUP)
 radio.on_received_string(on_received_string)
 
 # User Input Functionality
 input.on_button_pressed(Button.A, on_button_a_pressed)
-input.on_button_pressed(Button.B, on_button_b_pressed)
 input.on_button_pressed(Button.AB, on_button_ab_pressed)
 
 # start showing current mode on boot
 show_mode()
+set_light(True, False, False)
 basic.forever(on_forever)
